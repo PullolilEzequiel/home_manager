bfc10c437da718f08a96dcb9ffdc67ab695878a3
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2024-07-09 07:07:12 -0300

    (feat) Added logic to the command save - Both files and directories are copied, but the hide files make troubles

--- internal/command/save_command/command.go
+++ internal/command/save_command/command.go
@@ -1,7 +1,19 @@
 package savecommand
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func Execute(cmd *cobra.Command, args []string) {
+	var user, _ = os.UserHomeDir()
+	fmt.Println("Saving config files...")
+	configFolder := fmt.Sprintf("%s/.config/wizard_home", user)
+	dir, _ := os.MkdirTemp(configFolder, "saving_state")
 
+	PersistFiles(dir, configFolder)
+
+	//defer os.RemoveAll(dir)
 }
