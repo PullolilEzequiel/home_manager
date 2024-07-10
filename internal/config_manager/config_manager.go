1a880c1f428d83792c56b15f45888d2b547de9c2
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2024-07-10 04:58:38 -0300

    Implemented the method CreateTemporalFolder for Config

--- internal/config_manager/config_manager.go
+++ internal/config_manager/config_manager.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-type config struct {
+type __jsonConfig struct {
 	Repository_url     string   `json:repository_url`
 	Configs_to_persist []string `json:configs_to_persist`
 }
@@ -19,7 +19,7 @@ type Config struct {
 }
 
 func GetConfig() Config {
-	obj := config{}
+	obj := __jsonConfig{}
 	user, err := os.UserHomeDir()
 	panicCheck(err)
 	path := fmt.Sprintf("%s/.config/wizard_home", user)
@@ -46,4 +46,26 @@ func panicCheck(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func (c Config) Path() string          { return c.wizard_home_path }
+func (c Config) RepoUrl() string       { return c.repository_url }
+func (c Config) ConfigPaths() []string { return c.config_paths }
+
+/*
+Creates a temporal folder in the wizard_home folder with the patter passed for paramter and make action inside it and remove the folder
+@folderPattern string
+  - The pattern name
+
+@insideTemporalFolderAction func(folderName string) error
+  - Action performed within the temporary folder.
+*/
+func (c Config) CreateTemporalFolder(folderPattern string, insideTemporalFolderAction func(folderName string) error) error {
+	s, err := os.MkdirTemp(c.Path(), folderPattern)
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(s)
+
+	return insideTemporalFolderAction(s)
 }
