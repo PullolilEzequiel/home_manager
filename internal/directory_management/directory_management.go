319991e92a1e2dd07ccf03c5dfe5467ab98b3c2c
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2024-07-20 19:08:28 -0300

    (feat) Added CopyFolderOrFile function to directory_management module

--- internal/directory_management/directory_management.go
+++ internal/directory_management/directory_management.go
@@ -3,6 +3,7 @@ package directorymanagement
 import (
 	"fmt"
 	"os"
+	"path"
 )
 
 /*
@@ -46,4 +47,72 @@ func ReplaceFile(oldFile, sourceFile string) error {
 		return err
 	}
 	return os.Rename(sourceFile, oldFile)
+}
+
+/*
+Copy the file or folder from the directory passed by parameter
+@fileOrFolderPath -string- Path of  the file or folder to copy
+@newPath -string- The path of the folder to copy file or folder
+*/
+func CopyFolderOrFile(fileOrFolderPath, newPath string) error {
+	mode, err := os.Stat(fileOrFolderPath)
+	if err != nil {
+		return err
+	}
+	if mode.IsDir() {
+		return copyFolder(fileOrFolderPath, newPath)
+	} else {
+		return copyFile(fileOrFolderPath, newPath)
+	}
+}
+
+/*
+Copy the folder from the directory passed by parameter
+@folderPath -string- Path of  the folder to copy
+@newPath -string- The path of the folder to copy the folder
+
+! this not copy the .git folder, warning->
+*/
+func copyFolder(folderPath, newPath string) error {
+	name := path.Base(folderPath)
+	if name == ".git" {
+		return nil
+	}
+	dir := createFolder(newPath, name)
+	files, err := os.ReadDir(folderPath)
+	if err != nil {
+		return err
+	}
+	for _, f := range files {
+		pathOf := path.Join(folderPath, f.Name())
+		if err := CopyFolderOrFile(pathOf, dir); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+/*
+Copy the folder from the directory passed by parameter
+@filePath -string- Path of the file to copy
+@newPath -string- The path of the folder to copy the file
+*/
+func copyFile(filePath, newPath string) error {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+	pathToCopyFile := path.Join(newPath, path.Base(filePath))
+	if err := os.WriteFile(pathToCopyFile, content, 0666); err != nil {
+		return err
+	}
+	return nil
+}
+func createFolder(nPath, name string) string {
+	dir := path.Join(nPath, name)
+	if err := os.Mkdir(dir, 0777); err != nil {
+		panic(err)
+	}
+
+	return dir
 }
